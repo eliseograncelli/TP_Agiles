c437fc19d73f37d9e29f6234b12b9c53c63688d7
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-09-30 20:56:48 -0300

    animacion

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Hangman from './hangman.svelte';
+
+	let lives = 7;
+</script>
+
+<h1>TP AGILES</h1>
+
+<input type="number" bind:value={lives} />
+
+<Hangman {lives} />
