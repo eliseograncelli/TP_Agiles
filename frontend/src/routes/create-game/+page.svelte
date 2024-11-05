625a06e4a6664a770f686814865bd5814c541e43
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-11-05 20:29:57 -0300

    no hardcondign initial word

--- frontend/src/routes/create-game/+page.svelte
+++ frontend/src/routes/create-game/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-	let secret: string;
+	let word: string;
 	let url: string;
-	let base_url: string = "localhost:5173/play/";
+	let base_url: string = 'localhost:5173/play/';
 
 	function onCreateGame() {
-		fetch("http://localhost:8000/create-game", {
-    		method: "POST",
-    		headers: { "Content-Type": "application/json" },
-    		body: JSON.stringify({ word: "hola" })
+		fetch('http://localhost:8000/create-game', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ word })
 		})
-		.then(response => response.json())
-		.then(data => {
-			url = base_url + data.id;
-		})
-		.catch(error => console.error("Error:", error));
+			.then((response) => response.json())
+			.then((data) => {
+				url = base_url + data.id;
+			})
+			.catch((error) => console.error('Error:', error));
 	}
 </script>
+
 {#if url}
 	<p>url: {url}</p>
-	{:else }
-		<input type="text" bind:value={secret} />
-		<button on:click={onCreateGame}>Create Game</button>
+{:else}
+	<input type="text" bind:value={word} />
+	<button on:click={onCreateGame}>Create Game</button>
 {/if}
-
