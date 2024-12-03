977154ecfbfd28f1ebeb1a2d675cb9a72cafc454
eliseograncelli <eliseograncelli@gmail.com>
2024-12-03 19:49:33 -0300

    first AT

--- frontend/tests/test.ts
+++ frontend/tests/test.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+// Scenario: Iniciar partida
+test('Iniciar partida', async ({ page }) => {
+	// Given: ingreso la palabra "agil"
+	await page.goto('/create-game');
+	const input = await page.locator('input');
+	await input.fill('agil');
+
+	const button = await page.locator('button');
+
+	await Promise.allSettled([button.click(), page.waitForResponse((response) => response.ok())]);
+
+	//Then: debería ver el link a la partida
+	const anchor = await page.locator('a');
+	expect(anchor).toBeVisible();
 });
