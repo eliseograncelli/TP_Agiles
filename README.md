0e3c649caadd6e778587cf14a8075c5906874e9b
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-12-05 18:42:16 -0300

    todo list

---
+++ README.md
@@ -0,0 +1,21 @@
+
+Para hacer:
+- [X] Unit Tests usando TDD 
+- [x] Single Repository
+- [x] CI Server que solo compile (Github Actions, Azure DevOps, TravisCI, etc)
+- [x] CI Server ejecute los Unit Tests
+- [x] CI Server deploye a Producción
+- [x] CI Server ejecuta Análisis Estático de Código 
+- [] CI Server muestre resultados de Code Coverage
+- [] Desarrollar UI Web con Acceptance Test (al menos 4)
+- [] CI Server ejecute los AT
+
+Extras:
+- [] FEATURE: Adivinar palabra !!!
+- [] FEATURE: Que no sea case sensitive !!!
+- [] FEATURE: no mostrar palabra repetidas repetidas !!!
+- [] LLegar al 100% de TDD (97.1%)
+- [] Estandarizar los test del back 
+- [] Tener test de integracion ?????
+- [ ] Mejorar la UI ????????
+
---
+++ integration/api.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from "vitest";
+
+test("Adivnia letra correcta", async () => {
+  const { id } = await fetch("http://localhost:8000/create-game", {
+    method: "POST",
+    body: JSON.stringify({ word: "hola" }),
+  }).then((x) => x.json());
+
+  const res = await fetch("http://localhost:8000/guess-letter", {
+    method: "POST",
+    body: JSON.stringify({ gameId: id, letter: "h".charCodeAt(0) }),
+  }).then((x) => x.json());
+
+  expect(res).toEqual({ type: "correct", encoded: "h###" });
+});
+
+test("Adivnia letra incorrecta", async () => {
+  const { id } = await fetch("http://localhost:8000/create-game", {
+    method: "POST",
+    body: JSON.stringify({ word: "hola" }),
+  }).then((x) => x.json());
+
+  const res = await fetch("http://localhost:8000/guess-letter", {
+    method: "POST",
+    body: JSON.stringify({ gameId: id, letter: "p".charCodeAt(0) }),
+  }).then((x) => x.json());
+
+  expect(res).toEqual({ type: "wrong", lives: 6 });
+});
