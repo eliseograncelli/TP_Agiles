5dd3899fba3bc9b955d782e3446111b4b5b313e7
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-12-05 16:13:27 -0300

    refactor tests

--- frontend/tests/test.ts
+++ frontend/tests/test.ts
@@ -4,41 +4,35 @@ import { expect, test } from '@playwright/test';
 test('Iniciar partida', async ({ page }) => {
 	// Given: ingreso la palabra "agil"
 	await page.goto('/create-game');
-	const input = page.locator('input');
-	await input.fill('agil');
-
-	const button = page.locator('button');
-
-	await Promise.allSettled([button.click(), page.waitForResponse((response) => response.ok())]);
+	page.locator('input').fill('agil');
+	await Promise.allSettled([
+		page.locator('button').click(),
+		page.waitForResponse((response) => response.ok())
+	]);
 
 	//Then: debería ver el link a la partida
 	const anchor = page.locator('a');
 	expect(anchor).toBeVisible();
 
-	//Then: al hacer click debería ver la letra revelada en la palabra
+	//Then: al hacer click debería ver la pantalla de juego con la palabra oculta
 	await Promise.allSettled([anchor.click(), page.waitForResponse((response) => response.ok())]);
-
 	const dashes = await page.locator('.dash').all();
 	expect(dashes.length).toBe(4);
 });
 
 // Scenario: Ingresar letra válida
 test('Ingresar letra válida', async ({ page }) => {
-	// Given: ingreso la palabra "agil"
-
+	// Given: Partida iniciada con la palabra "agil"
 	await page.goto('/create-game');
-	const input = page.locator('input');
-	await input.fill('agil');
-
-	const button = page.locator('button');
-
-	await Promise.allSettled([button.click(), page.waitForResponse((response) => response.ok())]);
-
-	//Then: debería ver el link a la partida
-	const anchor = page.locator('a');
-	await Promise.allSettled([page.waitForResponse((response) => response.ok())]);
-
-	expect(anchor).toBeVisible();
+	page.locator('input').fill('agil');
+	await Promise.allSettled([
+		page.locator('button').click(),
+		page.waitForResponse((response) => response.ok())
+	]);
+	await Promise.allSettled([
+		page.locator('a').click(),
+		page.waitForResponse((response) => response.ok())
+	]);
 
 	// When: intento la letra "g"
 	// Then: debería ver la letra revelada en la palabra
